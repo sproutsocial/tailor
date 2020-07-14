cbc5cc71087032ff630a93f7f70c7b9c6848eb9f
Justin Conway <justin@conwaydev.com>
2020-07-14 15:40:08 -0500

    chore: export assets with new build tools

--- assets/js/dist/admin.js
+++ assets/js/dist/admin.js
@@ -1,241 +1,122 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-require( './shared/utility/ajax' );
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./assets/js/src/admin.js");
+/******/ })
+/************************************************************************/
+/******/ ({
 
-require( './admin/components/icon-kits' );
-},{"./admin/components/icon-kits":2,"./shared/utility/ajax":3}],2:[function(require,module,exports){
-/* window._l10n, window.nonces */
+/***/ "./assets/js/src/admin.js":
+/*!********************************!*\
+  !*** ./assets/js/src/admin.js ***!
+  \********************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
 
-var $ = window.jQuery,
-	l10n = window._l10n,
-	ajax = window.ajax;
+eval("__webpack_require__( /*! ./shared/utility/ajax */ \"./assets/js/src/shared/utility/ajax.js\" );\n\n__webpack_require__( /*! ./admin/components/icon-kits */ \"./assets/js/src/admin/components/icon-kits.js\" );\n\n//# sourceURL=webpack:///./assets/js/src/admin.js?");
 
-$( function() {
-	var $field = $( '.tailor-icon-kits' );
-	var frame = wp.media( {
-		button: {
-			text : l10n.selectKit
-		},
-		states: [
-			new wp.media.controller.Library({
-				title : l10n.selectKit,
-				library : wp.media.query( { type : [ 'application/zip' ] } ),
-				multiple : false,
-				date : false
-			} )
-		]
-	} );
+/***/ }),
 
-	frame.on( 'select', function( e ) {
-		var selection = frame.state().get( 'selection' ).toJSON();
-		var id = _.pluck( selection, 'id' );
+/***/ "./assets/js/src/admin/components/icon-kits.js":
+/*!*****************************************************!*\
+  !*** ./assets/js/src/admin/components/icon-kits.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-		$field
-			.find( '.spinner' )
-			.addClass( 'is-active' );
-		
-		ajax.send( 'tailor_add_icon_kit', {
-			data : {
-				id : id[0],
-				name : $field.data( 'name' ),
-				nonce : window._nonces.saveKit
-			},
+eval("/* window._l10n, window.nonces */\n\nvar $ = window.jQuery,\n\tl10n = window._l10n,\n\tajax = window.ajax;\n\n$( function() {\n\tvar $field = $( '.tailor-icon-kits' );\n\tvar frame = wp.media( {\n\t\tbutton: {\n\t\t\ttext : l10n.selectKit\n\t\t},\n\t\tstates: [\n\t\t\tnew wp.media.controller.Library({\n\t\t\t\ttitle : l10n.selectKit,\n\t\t\t\tlibrary : wp.media.query( { type : [ 'application/zip' ] } ),\n\t\t\t\tmultiple : false,\n\t\t\t\tdate : false\n\t\t\t} )\n\t\t]\n\t} );\n\n\tframe.on( 'select', function( e ) {\n\t\tvar selection = frame.state().get( 'selection' ).toJSON();\n\t\tvar id = _.pluck( selection, 'id' );\n\n\t\t$field\n\t\t\t.find( '.spinner' )\n\t\t\t.addClass( 'is-active' );\n\t\t\n\t\tajax.send( 'tailor_add_icon_kit', {\n\t\t\tdata : {\n\t\t\t\tid : id[0],\n\t\t\t\tname : $field.data( 'name' ),\n\t\t\t\tnonce : window._nonces.saveKit\n\t\t\t},\n\n\t\t\t/**\n\t\t\t * Display an error message when the request is unsuccessful.\n\t\t\t * \n\t\t\t * @since 1.0.0\n\t\t\t * \n\t\t\t * @param response\n\t\t\t */\n\t\t\terror : function( response ) {\n\t\t\t\tvar template = _.template( document.getElementById( 'tmpl-tailor-notice' ).innerHTML );\n\t\t\t\tvar $notice = $( template( { message: response.message } ) );\n\n\t\t\t\t$field\n\t\t\t\t\t.parent()\n\t\t\t\t\t.before( $notice );\n\n\t\t\t\t$notice.on( 'click button', function() {\n\t\t\t\t\t$notice\n\t\t\t\t\t\t.fadeOut()\n\t\t\t\t\t\t.off();\n\t\t\t\t} );\n\n\t\t\t\t$field\n\t\t\t\t\t.find( '.spinner' )\n\t\t\t\t\t.removeClass( 'is-active' );\n\t\t\t},\n\n\t\t\t/**\n\t\t\t * Update the list of icon kits when the request is successful.\n\t\t\t * \n\t\t\t * @since 1.0.0\n\t\t\t * \n\t\t\t * @param response\n\t\t\t */\n\t\t\tsuccess : function( response ) {\n\t\t\t\t$field.html( $( response ).html() );\n\t\t\t}\n\n\t\t} );\n\t} );\n\n\t$field\n\n\t\t// Open the Media Library frame\n\t\t.on( 'click', '.js-select', function( e ) {\n\t\t\tframe.open();\n\t\t} )\n\n\t\t// Delete an icon kit\n\t\t.on( 'click', '.js-delete', function( e ) {\n\n\t\t\t$field\n\t\t\t\t.find( '.spinner' )\n\t\t\t\t.addClass( 'is-active' );\n\n\t\t\tajax.send( 'tailor_delete_icon_kit', {\n\t\t\t\tdata : {\n\t\t\t\t\tid : this.getAttribute( 'data-id' ),\n\t\t\t\t\tname : $field.data( 'name' ),\n\t\t\t\t\tnonce : window._nonces.deleteKit\n\t\t\t\t},\n\n\t\t\t\t/**\n\t\t\t\t * Update the icon kit when the request is successful.\n\t\t\t\t * \n\t\t\t\t * @since 1.0.0\n\t\t\t\t * \n\t\t\t\t * @param response\n\t\t\t\t */\n\t\t\t\tsuccess : function( response ) {\n\t\t\t\t\t$field.html( $( response ).html() );\n\t\t\t\t}\n\t\t\t} );\n\t\n\t\t\te.preventDefault();\n\t} );\n} );\n\n//# sourceURL=webpack:///./assets/js/src/admin/components/icon-kits.js?");
 
-			/**
-			 * Display an error message when the request is unsuccessful.
-			 * 
-			 * @since 1.0.0
-			 * 
-			 * @param response
-			 */
-			error : function( response ) {
-				var template = _.template( document.getElementById( 'tmpl-tailor-notice' ).innerHTML );
-				var $notice = $( template( { message: response.message } ) );
+/***/ }),
 
-				$field
-					.parent()
-					.before( $notice );
+/***/ "./assets/js/src/shared/utility/ajax.js":
+/*!**********************************************!*\
+  !*** ./assets/js/src/shared/utility/ajax.js ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-				$notice.on( 'click button', function() {
-					$notice
-						.fadeOut()
-						.off();
-				} );
+eval("/**\n * window.ajax\n *\n * Simple AJAX utility module.\n *\n * @class\n */\nvar $ = jQuery,\n    Ajax;\n\nAjax = {\n\n    url : window.ajaxurl,\n\n    /**\n     * Sends a POST request to WordPress.\n     *\n     * @param  {string} action The slug of the action to fire in WordPress.\n     * @param  {object} data   The data to populate $_POST with.\n     * @return {$.promise}     A jQuery promise that represents the request.\n     */\n    post : function( action, data ) {\n        return ajax.send( {\n            data: _.isObject( action ) ? action : _.extend( data || {}, { action: action } )\n        } );\n    },\n\n    /**\n     * Sends a POST request to WordPress.\n     *\n     * Use with wp_send_json_success() and wp_send_json_error().\n     *\n     * @param  {string} action  The slug of the action to fire in WordPress.\n     * @param  {object} options The options passed to jQuery.ajax.\n     * @return {$.promise}      A jQuery promise that represents the request.\n     */\n    send : function( action, options ) {\n\n        if ( _.isObject( action ) ) {\n            options = action;\n        }\n        else {\n            options = options || {};\n            options.data = _.extend( options.data || {}, {\n                action : action,\n                tailor : 1\n            } );\n        }\n\n        options = _.defaults( options || {}, {\n            type : 'POST',\n            url : ajax.url,\n            context : this\n        } );\n\n        return $.Deferred( function( deferred ) {\n\n            if ( options.success ) {\n                deferred.done( options.success );\n            }\n\n            var onError = options.error ? options.error : ajax.onError;\n            deferred.fail( onError );\n\n            delete options.success;\n            delete options.error;\n\n            $.ajax( options )\n                .done( function( response ) {\n\n                    // Treat a response of `1` as successful for backwards compatibility with existing handlers.\n                    if ( response === '1' || response === 1 ) {\n                        response = { success: true };\n                    }\n                    if ( _.isObject( response ) && ! _.isUndefined( response.success ) ) {\n                        deferred[ response.success ? 'resolveWith' : 'rejectWith' ]( this, [ response.data ] );\n                    }\n                    else {\n                        deferred.rejectWith( this, [ response ] );\n                    }\n                } )\n                .fail( function() {\n                    deferred.rejectWith( this, arguments );\n                } );\n\n        } ).promise();\n    },\n\n    /**\n     * General error handler for AJAX requests.\n     *\n     * @since 1.0.0\n     *\n     * @param response\n     */\n    onError : function( response ) {\n\n        // Print the error to the console if the Notify feature is unavailable\n        if ( ! Tailor.Notify ) {\n            console.error( response );\n            return;\n        }\n\n        if ( response && response.hasOwnProperty( 'message' ) ) {  // Display the error from the server\n            Tailor.Notify( response.message );\n        }\n        else if ( '0' == response ) {  // Session expired\n            Tailor.Notify( window._l10n.expired );\n        }\n        else if ( '-1' == response ) {  // Invalid nonce\n            Tailor.Notify( window._l10n.invalid );\n        }\n        else {  // General error condition\n            Tailor.Notify( window._l10n.error );\n        }\n    }\n};\n\nwindow.ajax = Ajax;\n\nmodule.exports = Ajax;\n\n//# sourceURL=webpack:///./assets/js/src/shared/utility/ajax.js?");
 
-				$field
-					.find( '.spinner' )
-					.removeClass( 'is-active' );
-			},
+/***/ })
 
-			/**
-			 * Update the list of icon kits when the request is successful.
-			 * 
-			 * @since 1.0.0
-			 * 
-			 * @param response
-			 */
-			success : function( response ) {
-				$field.html( $( response ).html() );
-			}
-
-		} );
-	} );
-
-	$field
-
-		// Open the Media Library frame
-		.on( 'click', '.js-select', function( e ) {
-			frame.open();
-		} )
-
-		// Delete an icon kit
-		.on( 'click', '.js-delete', function( e ) {
-
-			$field
-				.find( '.spinner' )
-				.addClass( 'is-active' );
-
-			ajax.send( 'tailor_delete_icon_kit', {
-				data : {
-					id : this.getAttribute( 'data-id' ),
-					name : $field.data( 'name' ),
-					nonce : window._nonces.deleteKit
-				},
-
-				/**
-				 * Update the icon kit when the request is successful.
-				 * 
-				 * @since 1.0.0
-				 * 
-				 * @param response
-				 */
-				success : function( response ) {
-					$field.html( $( response ).html() );
-				}
-			} );
-	
-			e.preventDefault();
-	} );
-} );
-},{}],3:[function(require,module,exports){
-/**
- * window.ajax
- *
- * Simple AJAX utility module.
- *
- * @class
- */
-var $ = jQuery,
-    Ajax;
-
-Ajax = {
-
-    url : window.ajaxurl,
-
-    /**
-     * Sends a POST request to WordPress.
-     *
-     * @param  {string} action The slug of the action to fire in WordPress.
-     * @param  {object} data   The data to populate $_POST with.
-     * @return {$.promise}     A jQuery promise that represents the request.
-     */
-    post : function( action, data ) {
-        return ajax.send( {
-            data: _.isObject( action ) ? action : _.extend( data || {}, { action: action } )
-        } );
-    },
-
-    /**
-     * Sends a POST request to WordPress.
-     *
-     * Use with wp_send_json_success() and wp_send_json_error().
-     *
-     * @param  {string} action  The slug of the action to fire in WordPress.
-     * @param  {object} options The options passed to jQuery.ajax.
-     * @return {$.promise}      A jQuery promise that represents the request.
-     */
-    send : function( action, options ) {
-
-        if ( _.isObject( action ) ) {
-            options = action;
-        }
-        else {
-            options = options || {};
-            options.data = _.extend( options.data || {}, {
-                action : action,
-                tailor : 1
-            } );
-        }
-
-        options = _.defaults( options || {}, {
-            type : 'POST',
-            url : ajax.url,
-            context : this
-        } );
-
-        return $.Deferred( function( deferred ) {
-
-            if ( options.success ) {
-                deferred.done( options.success );
-            }
-
-            var onError = options.error ? options.error : ajax.onError;
-            deferred.fail( onError );
-
-            delete options.success;
-            delete options.error;
-
-            $.ajax( options )
-                .done( function( response ) {
-
-                    // Treat a response of `1` as successful for backwards compatibility with existing handlers.
-                    if ( response === '1' || response === 1 ) {
-                        response = { success: true };
-                    }
-                    if ( _.isObject( response ) && ! _.isUndefined( response.success ) ) {
-                        deferred[ response.success ? 'resolveWith' : 'rejectWith' ]( this, [ response.data ] );
-                    }
-                    else {
-                        deferred.rejectWith( this, [ response ] );
-                    }
-                } )
-                .fail( function() {
-                    deferred.rejectWith( this, arguments );
-                } );
-
-        } ).promise();
-    },
-
-    /**
-     * General error handler for AJAX requests.
-     *
-     * @since 1.0.0
-     *
-     * @param response
-     */
-    onError : function( response ) {
-
-        // Print the error to the console if the Notify feature is unavailable
-        if ( ! Tailor.Notify ) {
-            console.error( response );
-            return;
-        }
-
-        if ( response && response.hasOwnProperty( 'message' ) ) {  // Display the error from the server
-            Tailor.Notify( response.message );
-        }
-        else if ( '0' == response ) {  // Session expired
-            Tailor.Notify( window._l10n.expired );
-        }
-        else if ( '-1' == response ) {  // Invalid nonce
-            Tailor.Notify( window._l10n.invalid );
-        }
-        else {  // General error condition
-            Tailor.Notify( window._l10n.error );
-        }
-    }
-};
-
-window.ajax = Ajax;
-
-module.exports = Ajax;
-},{}]},{},[1]);
+/******/ });
