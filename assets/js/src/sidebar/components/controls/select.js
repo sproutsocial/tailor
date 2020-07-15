90c9f36c1d091ad9c532c36b81d0dcb5aa1b24ca
Justin Conway <justin@conwaydev.com>
2020-07-14 21:12:54 -0500

    fix: start to allow for editing with data components

--- assets/js/src/sidebar/components/controls/select.js
+++ assets/js/src/sidebar/components/controls/select.js
@@ -24,10 +24,43 @@ SelectControl = AbstractControl.extend( {
     },
 
     templateHelpers : {
-        
+
         selected : function( media, key ) {
             return key === this.values[ media ] ? 'selected' : '';
+        },
+
+        edit : (media, key) => {
+            if (key === this.values[ media ]) {
+                return `<a href="/edit.php?p=${key}">Edit</a>`;
+            }
         }
+    },
+
+    /**
+     * Initializes the selectize instance(s) and updates the media-query based control groups when the control is rendered.
+     *
+     * @since 1.7.2
+     */
+    onRender : function() {
+        console.log(this.getValues());
+        _.each(this.getValues(), ( value, media ) => {
+            var $field = this.ui.input.filter( '[name^="' + media + '"]' );
+
+            _.each($field, () => $field.selectize());
+        });
+
+        this.updateControlGroups();
+    },
+
+    /**
+     * Destroys the selectize instance(s).
+     *
+     * @since 1.7.2
+     */
+    onDestroy : function() {
+        _.each( this.getValues(), function( value, media ) {
+            this.ui.input.filter( '[name^="' + media + '"]' ).selectize( 'destroy' );
+        }, this );
     }
 
 } );
